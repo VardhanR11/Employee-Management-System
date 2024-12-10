83b739085fd573154ce8ba20f34eec5a68f32e8c
VardhanR11 <vardhanrathore32@gmail.com>
2024-12-10 23:55:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './components/Auth/Login'
+import AdminDashboard from './components/Dashboard/'
+
+const App = () => {
+  return (
+    <>
+    <Login/>
+  
+    </>
+  )
+}
+
+export default App
